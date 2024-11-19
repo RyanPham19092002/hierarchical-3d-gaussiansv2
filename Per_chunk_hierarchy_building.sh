0deb9e7822e82e96c107ad9de5f2c77a89ed27c0
RyanPham19092002 <phamthanhphat711@gmail.com>
2024-11-19 03:05:07 +0000

    first commit

---
+++ Per_chunk_hierarchy_building.sh
@@ -0,0 +1,5 @@
+submodules/gaussianhierarchy/build/GaussianHierarchyCreator \
+ /home/vinai/Workspace/phat-intern-dev/VinAI/hierarchical-3d-gaussians/data/example_dataset/output/trained_chunks/1_0/point_cloud/iteration_30000/point_cloud.ply \
+ /home/vinai/Workspace/phat-intern-dev/VinAI/hierarchical-3d-gaussians/data/example_dataset/camera_calibration/chunks/1_0 \
+ /home/vinai/Workspace/phat-intern-dev/VinAI/hierarchical-3d-gaussians/data/example_dataset/output/trained_chunks/1_0 \
+ /home/vinai/Workspace/phat-intern-dev/VinAI/hierarchical-3d-gaussians/data/example_dataset/output/scaffold
