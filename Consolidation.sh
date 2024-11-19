e0c0f6b8195a0d016f2b5ebe8469480091418760
RyanPham19092002 <phamthanhphat711@gmail.com>
2024-11-19 03:04:23 +0000

    first commit

---
+++ Consolidation.sh
@@ -0,0 +1,4 @@
+submodules/gaussianhierarchy/build/GaussianHierarchyMerger \
+ /home/vinai/Workspace/phat-intern-dev/VinAI/hierarchical-3d-gaussians/data/example_dataset/output/trained_chunks "0" \
+ /home/vinai/Workspace/phat-intern-dev/VinAI/hierarchical-3d-gaussians/data/example_dataset/camera_calibration/chunks \
+ 0_0 1_0
